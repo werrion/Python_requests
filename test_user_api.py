1c14a2e740b43ef685b3f9589d8aef456e3d9189
oleg.krivov <oleg.krivov@ab-games.com>
2021-06-17 15:42:40 +0300

    remake test - allure info

--- test_user_api.py
+++ test_user_api.py
@@ -3,23 +3,22 @@ import requests
 from request_example import *
 
 
-@allure.story("Тест POST запрос")
+@allure.story("Тесты - POST запрос")
 @allure.step("Проверка ответа POST запроса ")
 def test_post():
     response = requests.request("POST", url, headers=headers, data=payload)
     print(response.text)
 
 
+@allure.story("Тесты для проверки Ассертейшенов")
 @allure.step("Тест Ok статус")
 def test_ok():
     assert 1 == 1
 
-"""@allure.step("Тест fail статус")
+
+@allure.step("Тест fail статус")
 def test_fail():
     assert 1 == 2
-    
-"""
-
 
 
 @allure.step("Тест Ok1 статус")
