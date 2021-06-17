b57a5edbf83335907f7f66fcd8e17086a83040cd
oleg.krivov <oleg.krivov@ab-games.com>
2021-06-17 15:53:33 +0300

    add second test files

--- additonal_test.py
+++ additonal_test.py
@@ -5,22 +5,22 @@ from request_example import *
 
 @allure.story("Тесты - POST запрос")
 @allure.step("Проверка ответа POST запроса ")
-def test_post():
+def test_post1():
     response = requests.request("POST", url, headers=headers, data=payload)
     print(response.text)
 
 
 @allure.story("Тесты для проверки Ассертейшенов")
 @allure.step("Тест Ok статус")
-def test_ok():
+def test_ok11():
     assert 1 == 1
 
 @allure.story("Тесты для проверки Ассертейшенов")
 @allure.step("Тест fail статус")
-def test_fail():
+def test_fail1():
     assert 1 == 2
 
 @allure.story("Тесты для проверки Ассертейшенов")
 @allure.step("Тест Ok1 статус")
-def test_ok1():
+def test_ok111():
     assert 2 == 2
