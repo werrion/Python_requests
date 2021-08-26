95b60268d02c45b66ba85526a6bd2b81fe986f23
oleg.krivov <oleg.krivov@ab-games.com>
2021-08-26 11:57:20 +0300

    allure test story

--- test_user_api.py
+++ test_user_api.py
@@ -15,12 +15,12 @@ def test_post():
 def test_ok():
     assert 1 == 1
 
-@allure.story("Тесты для проверки Ассертейшенов")
+
 @allure.step("Тест fail статус")
 def test_fail():
     assert 1 == 2
 
-@allure.story("Тесты для проверки Ассертейшенов")
+
 @allure.step("Тест Ok1 статус")
 def test_ok1():
     assert 2 == 2
